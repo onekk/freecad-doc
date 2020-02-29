a15c5bf80ac2da25663bc0269952a34f141a1a59
onekk <carlo.dormeletti@gmail.com>
2020-02-29 10:50:46 +0100

    prima bozza del documento

---
+++ listati/sc-base.py
@@ -0,0 +1,45 @@
+#
+"""sc-base.py
+
+   This code was written as an sample code 
+   in "FreeCAD Scripting Guide" 
+     
+   Author: Carlo Dormeletti
+   Copyright: 2020
+   Licence: CC BY-NC-ND 4.0 IT 
+"""
+
+import FreeCAD
+from FreeCAD import Base, Vector
+import Part
+from math import pi, sin, cos
+
+DOC = FreeCAD.activeDocument()
+DOC_NAME = "Pippo"
+
+def clear_doc():
+    """
+    Clear the active document deleting all the objects
+    """
+    for obj in DOC.Objects:
+        DOC.removeObject(obj.Name)
+
+def setview():
+    """Rearrange View"""
+    FreeCAD.Gui.SendMsgToActiveView("ViewFit")
+    FreeCAD.Gui.activeDocument().activeView().viewAxometric()
+
+
+if DOC is None:
+    FreeCAD.newDocument(DOC_NAME)
+    FreeCAD.setActiveDocument(DOC_NAME)
+    DOC = FreeCAD.activeDocument()
+
+else:
+
+    clear_doc()
+
+
+# EPS= tolerance to uset to cut the parts
+EPS = 0.10
+EPS_C = EPS * -0.5
